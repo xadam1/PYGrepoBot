6d602df0024e4446743742da4957757c3511762e
Jan Adam <xadam1@fi.muni.cz>
2019-04-26 15:31:13 +0200

    Added new farm_btn_recognition, changed get_btn() to separate func, fixed cannot locate image

--- grepo_bot.py
+++ grepo_bot.py
@@ -1,13 +1,23 @@
 import pyautogui
 import time
-from random import randint
+import random
 
 
-def collect_resources_on_island():
-    # get type of farming (1 = 5 mins, 2 = 10 mins)
-    collect_type = int(input("Farm every 5 mins [1] / 10 mins [2]: "))
+def collect_resources_on_island(farm_duration):
+    """
+    Loops through each farm village on island, and collect resources from each one of them
 
-    print("Please select active Grepolis window!")
+    :param farm_duration: int type representing what type of farming should be used
+    :return: nothing, collects resources from villages
+    """
+    # Create dictionary with images
+    collect_dict = {1: 'images/quick_farm_btn.png',
+                    2: 'TODO'}
+
+    # set img path to desired type of pic
+    collect_type = collect_dict[farm_duration]
+
+    print("Please select active Grepolis browser window!")
 
     time.sleep(3)
 
@@ -15,44 +25,76 @@ def collect_resources_on_island():
     pyautogui.press('space')
     print("Centering on Island...")
 
-    # Click on resource
+    time.sleep(1)
+
+    # Opens collect from village window
     pyautogui.click('images/resources.png')
     print("Found village with resources.")
 
-    btn_collect, btn_next_village = get_cords_of_btns(collect_type)
+    # Find buttons and save the positions
+    btn_collect = get_cords_of_btn(collect_type)
+    btn_next_village = get_cords_of_btn('images/next_farm_village_button.png')
 
+    print("Collecting your goodies!")
     for _ in range(6):
         pyautogui.click(btn_collect)
-        print("Collected goodies for 10min.")
-        time.sleep(randint(1, 2))
+
+        # sleeps for (0.3 - 1.3 sec) - rounded random on 2 digits (0.00 - 1.00) + 0.3 sec
+        time.sleep(round(random.random(), 2) + 0.3)
 
         pyautogui.click(btn_next_village)
-        print("Visiting next village...")
-        time.sleep(randint(1, 2))
+
+        time.sleep(round(random.random(), 2) + 0.3)
 
     pyautogui.press('escape')
-    print("Collected from all villages on this island.")
+    print("Successfully collected resources from villages on this island, boss!")
 
 
-def get_cords_of_btns(collect_type):
-    # 1 = 5 min, 2 = 10 min
-    collect_dict = {1: 'images/5min_farm_village_button.png', 2: 'images/10min_farm_village_button.png'}
+def get_cords_of_btn(btn_picture):
+    """
+    Just locate the picture passed as a parameter
 
-    # get location of picture collect
-    location_of_collect = pyautogui.locateOnScreen(collect_dict[collect_type])
+    :param btn_picture: .png picture of btn that should be found
+    :return: x,y cords to click on the desired button
+    """
 
-    # convert to x,y format
-    btn_collect = pyautogui.center(location_of_collect)
-    print("collect btn found: ", btn_collect)
-
-    # get location of picture next_arrow_farm_village
-    location_of_next_village_btn = pyautogui.locateOnScreen('images/next_farm_village_button.png')
+    # get btn position
+    square_location_btn = pyautogui.locateOnScreen(btn_picture, 5)
 
     # convert to x,y format
-    btn_next_village = pyautogui.center(location_of_next_village_btn)
-    print("Next village btn found: ", btn_next_village)
+    btn_position = pyautogui.center(square_location_btn)
 
-    return btn_collect, btn_next_village
+    print("DEBUG: found btn_location:\t", btn_position)
+
+    return btn_position
+
+
+def get_input_init():
+    """
+    Quick function to get user input of wanted farming mode
+
+    :return: int type representing farming mode
+    """
+    # Get input from user for type of farming
+    collect_type = int(input("What type of farming do you want to start?\n"
+                             "[1]: Short (every 10min)\n"
+                             "[2]: Longer ... not implemented yet ->\t"))
+    return collect_type
+
+
+def main():
+    """
+    Main function, starts when Bot is executed
+
+    :return: not determined yet, probably nothing
+    """
+
+    print("Welcome to farming GrepoBot!\n\n")
+
+    # Get type of desired farming
+    collect_duration = get_input_init()
+
+    collect_resources_on_island(collect_duration)
 
 
 """
@@ -75,10 +117,5 @@ def get_number_of_farm_villages():
 
     return number_of_farm_villages
 """
-
-
-def main():
-    collect_resources_on_island()
-
 
 main()
